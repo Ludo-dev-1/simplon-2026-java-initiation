c5be6fbb113a5c3ddb592bde5dfc86a0c7d47b2a
THIBAUD Ludovic <ludovic.thibaud@maif.fr>
2026-02-09 13:30:53 +0100

    ex3 OK

--- src/main/java/fr/simplon/exercises/Exercise03ConditionalStatements.java
+++ src/main/java/fr/simplon/exercises/Exercise03ConditionalStatements.java
@@ -14,7 +14,10 @@ public class Exercise03ConditionalStatements {
      * @return le plus grand des deux nombres
      */
     public int max(int a, int b) {
-        throw new UnsupportedOperationException();
+        if (a>b){
+            return a;
+        } else return b;
+        //throw new UnsupportedOperationException();
 
     }
     
@@ -24,7 +27,10 @@ public class Exercise03ConditionalStatements {
      * @return true si le nombre est positif (> 0), false sinon
      */
     public boolean isPositive(int number) {
-        throw new UnsupportedOperationException();
+        if (number>0){
+            return true;
+        }else return false;
+        //throw new UnsupportedOperationException();
 
     }
     
@@ -34,7 +40,9 @@ public class Exercise03ConditionalStatements {
      * @return la valeur absolue du nombre
      */
     public int absoluteValue(int number) {
-        throw new UnsupportedOperationException();
+        int abs = Math.abs(number);
+        return abs;
+        //throw new UnsupportedOperationException();
 
     }
     
@@ -44,7 +52,10 @@ public class Exercise03ConditionalStatements {
      * @return true si le nombre est pair, false sinon
      */
     public boolean isEven(int number) {
-        throw new UnsupportedOperationException();
+        if ( number %2 ==0){
+            return true;
+        }else return false;
+        //throw new UnsupportedOperationException();
 
     }
     
@@ -54,7 +65,21 @@ public class Exercise03ConditionalStatements {
      * @return "A" si >= 90, "B" si >= 80, "C" si >= 70, "D" si >= 60, "F" sinon
      */
     public String getGrade(int score) {
-        throw new UnsupportedOperationException();
+       if (score >= 90 ){
+        String s = "A";
+           return s;
+       } else if(score >=80){
+        String s = "B";
+           return s;
+       }else if(score >=70){
+        String s = "C";
+           return s;
+       }else if(score >=60){
+        String s = "D";
+           return s;
+       }else return "F";
+    
+       // throw new UnsupportedOperationException();
 
     }
 }
