a09c7b8b2887acd3d253ffec7bd8cc8a3bd39509
THIBAUD Ludovic <ludovic.thibaud@maif.fr>
2026-02-10 09:28:30 +0100

    ex5 OK

--- src/main/java/fr/simplon/exercises/Exercise05ArraysBasics.java
+++ src/main/java/fr/simplon/exercises/Exercise05ArraysBasics.java
@@ -1,61 +1,97 @@
 package fr.simplon.exercises;
 
+import java.util.Arrays;
+
 /**
  * Exercice 5: Tableaux - Opérations de base
  * 
  * Objectif: Apprendre à manipuler des tableaux
  */
 public class Exercise05ArraysBasics {
-    
+
     /**
      * Crée un tableau contenant les nombres de 1 à n
+     * 
      * @param n la taille du tableau
      * @return un tableau [1, 2, 3, ..., n]
      */
     public int[] createSequence(int n) {
-        throw new UnsupportedOperationException();
+
+        int[] tab = new int[n];
+
+        for (int i = 0; i < tab.length; i++) {
+
+            tab[i] = i + 1;
+        }
+        return tab;
+
+        // throw new UnsupportedOperationException();
 
     }
-    
+
     /**
      * Retourne le premier élément d'un tableau
+     * 
      * @param array le tableau
      * @return le premier élément
      */
     public int getFirstElement(int[] array) {
-        throw new UnsupportedOperationException();
+
+        return array[0];
+        // throw new UnsupportedOperationException();
 
     }
-    
+
     /**
      * Retourne le dernier élément d'un tableau
+     * 
      * @param array le tableau
      * @return le dernier élément
      */
     public int getLastElement(int[] array) {
-        throw new UnsupportedOperationException();
+        int i = array.length - 1;
+
+        return array[i];
+
+        // throw new UnsupportedOperationException();
 
     }
-    
+
     /**
      * Compte le nombre d'occurrences d'une valeur dans un tableau
+     * 
      * @param array le tableau
      * @param value la valeur à chercher
      * @return le nombre d'occurrences
      */
     public int countOccurrences(int[] array, int value) {
-        throw new UnsupportedOperationException();
+        int count = 0;
 
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                count++;
+            }
+        }
+
+        return count;
+        // throw new UnsupportedOperationException();
     }
-    
+
     /**
      * Vérifie si un tableau contient une valeur
+     * 
      * @param array le tableau
      * @param value la valeur à chercher
      * @return true si la valeur est présente, false sinon
      */
     public boolean contains(int[] array, int value) {
-        throw new UnsupportedOperationException();
 
+        for (int i : array) {
+            if (i == value) {
+                return true;
+            }
+            // throw new UnsupportedOperationException();
+        }
+        return false;
     }
 }
